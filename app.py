1329c4daee37739b3ef4e0d79d356960833faab4
Prabhu Pechimuthu <pechimuthu369@gmail.com>
2021-10-07 21:18:11 +0100

    html changes

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/weatherfinder')
+def weatherfinder():
+    return render_template('weatherfinder.html')
+
+@app.route('/objectfinder')
+def objectfinder():
+    return render_template('objectfinder.html')
+
+@app.route('/greet')
 def hello():
     return 'Hello, World!'
