4a13fb456d7bd15ae834dffaa258910324573419
pechimuthu369 <37926169+pechimuthu369@users.noreply.github.com>
2021-11-07 10:33:52 +0000

    test

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import imghdr
-from flask import Flask,render_template,send_from_directory,abort,jsonify,request
 import requests
 import os
+import json
+from flask import Flask,render_template,send_from_directory,abort,jsonify,request
+from flask_cors import CORS, cross_origin
 from werkzeug.utils import secure_filename
+
+
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.debug = True
+
 app.config['API_KEY'] = "acc_a6910ab041ae060"
 app.config['API_SECRET'] = "2cf42c857875adfcc4fbc3bfe72c3703"
-
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 app.config['UPLOAD_PATH'] = 'images'
@@ -35,19 +41,36 @@ def objectfinder():
 def hello():
     return 'Hello, World!'
 
-@app.route('/uploadfiles' ,methods=["POST","GET"])
-def uploadfiles():        
-    uploaded_file = request.files['file']
-    filename = secure_filename(uploaded_file.filename)    
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        #if file_ext not in app.config['UPLOAD_EXTENSIONS'] or file_ext != validate_image(uploaded_file.stream):
-        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-            abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))    
-        file_path = os.path.join(app.config['UPLOAD_PATH'] +"/" + filename)
-        print(file_ext)        
-    return jsonify(filepath="hohohoohhoh")
+@app.route("/uploadajax", methods=["GET", "POST"])
+@cross_origin()
+def ProcessImage():
+    if request.method == "POST":        
+        #print(request.files)
+        try:
+            if 'file' in request.files:
+                uploaded_file = request.files['file']
+                filename = secure_filename(uploaded_file.filename)    
+                if filename != '':
+                    file_ext = os.path.splitext(filename)[1]
+                    if file_ext not in app.config['UPLOAD_EXTENSIONS'] or file_ext != validate_image(uploaded_file.stream):                    
+                        abort(400)
+                    file_path = os.path.join(app.config['UPLOAD_PATH'], filename)
+                    #uploaded_file.save(file_path)     
+                    image_abs_path = os.path.abspath(file_path)
+                    #print(image_abs_path)                            
+                    #response = requests.post(
+                        #'https://api.imagga.com/v2/uploads',
+                        #auth=(app.config['API_KEY'], app.config['API_SECRET']),
+                        #files={'image': open(image_abs_path, 'rb')}).json()                                                                                                  
+                    #print(response["result"]["upload_id"])           
+                    upid = "i080cc37faad0f1aa95da0add64K8Sc9"        
+                    imageresponse = requests.get(
+                        'https://api.imagga.com/v2/tags?image_upload_id	=%s' % upid,
+                        auth=(app.config['API_KEY'], app.config['API_SECRET'])).json()
+                    return imageresponse
+        except Exception as e:
+            print(e)
+        return jsonify("Ok")
 
 @app.route('/uploads/<filename>')
 def upload(filename):
