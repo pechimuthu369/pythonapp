7ce59d000afbecd13f72df6e7531cc5dd140638c
pechimuthu369 <37926169+pechimuthu369@users.noreply.github.com>
2021-11-16 16:41:43 +0000

    facefinder demo

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from flask import Flask,render_template,send_from_directory,abort,jsonify,reques
 from flask_cors import CORS, cross_origin
 from werkzeug.utils import secure_filename
 
+import cv2
+import sys
+
+
 
 app = Flask(__name__,static_url_path="/static", static_folder="static")
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -45,7 +49,7 @@ def facialfinder():
 def hello():
     return 'Hello, World!'
 
-@app.route("/uploadajax", methods=["GET", "POST"])
+@app.route("/objectupload", methods=["GET", "POST"])
 @cross_origin()
 def ProcessImage():
     if request.method == "POST":        
@@ -67,6 +71,71 @@ def ProcessImage():
                             auth=(app.config['API_KEY'], app.config['API_SECRET']),
                             files={'image': open(image_abs_path, 'rb')}).json()                                                                                                                                         
                     return response
+        except Exception as e:
+            print(e)            
+        return jsonify("Ok")
+
+@app.route("/facialupload", methods=["GET", "POST"])
+@cross_origin()
+def FacialImage():
+    if request.method == "POST":                
+        try:
+            s = 0
+            if len(sys.argv) > 1:
+                s = sys.argv[1]
+
+            source = cv2.VideoCapture(s)
+
+            win_name = 'Camera Preview'
+            cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+
+            net = cv2.dnn.readNetFromCaffe("deploy.prototxt",
+                                        "res10_300x300_ssd_iter_140000_fp16.caffemodel")
+            # Model parameters
+            in_width = 300
+            in_height = 300
+            mean = [104, 117, 123]
+            conf_threshold = 0.7
+
+            while cv2.waitKey(1) != 27:
+                has_frame, frame = source.read()
+                if not has_frame:
+                    break
+                frame = cv2.flip(frame,1)
+                frame_height = frame.shape[0]
+                frame_width = frame.shape[1]
+
+                # Create a 4D blob from a frame.
+                blob = cv2.dnn.blobFromImage(frame, 1.0, (in_width, in_height), mean, swapRB = False, crop = False)
+                # Run a model
+                net.setInput(blob)
+                detections = net.forward()
+
+                for i in range(detections.shape[2]):
+                    confidence = detections[0, 0, i, 2]
+                    if confidence > conf_threshold:
+                        x_left_bottom = int(detections[0, 0, i, 3] * frame_width)
+                        y_left_bottom = int(detections[0, 0, i, 4] * frame_height)
+                        x_right_top = int(detections[0, 0, i, 5] * frame_width)
+                        y_right_top = int(detections[0, 0, i, 6] * frame_height)
+
+                        cv2.rectangle(frame, (x_left_bottom, y_left_bottom), (x_right_top, y_right_top), (0, 255, 0))
+                        label = "Confidence: %.4f" % confidence
+                        label_size, base_line = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+                        cv2.rectangle(frame, (x_left_bottom, y_left_bottom - label_size[1]),
+                                            (x_left_bottom + label_size[0], y_left_bottom + base_line),
+                                            (255, 255, 255), cv2.FILLED)
+                        cv2.putText(frame, label, (x_left_bottom, y_left_bottom),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+                t, _ = net.getPerfProfile()
+                label = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
+                cv2.putText(frame, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                cv2.imshow(win_name, frame)
+
+                source.release()
+                cv2.destroyWindow(win_name)
         except Exception as e:
             print(e)            
         return jsonify("Ok")
